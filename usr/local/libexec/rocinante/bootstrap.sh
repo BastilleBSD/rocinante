6f4faf5b0f4a94c478fd1883349501912e0e3654
Bennie <bennie@o0.wtf>
2023-02-15 21:09:55 +0100

    bootstrap.sh: added new command line parameter digestion
    bootstrap.sh: added --single-branch and --branch [branch] to git clone

--- usr/local/libexec/rocinante/bootstrap.sh
+++ usr/local/libexec/rocinante/bootstrap.sh
@@ -73,6 +73,7 @@ fetch_template() {
     _url=${ROCINANTE_TEMPLATE_URL}
     _user=${ROCINANTE_TEMPLATE_USER}
     _repo=${ROCINANTE_TEMPLATE_REPO}
+    _branch=${ROCINANTE_TEMPLATE_BRANCH}
     _template=${rocinante_templatesdir}/${_user}/${_repo}
     _template_user=${rocinante_templatesdir}/${_user}
 
@@ -100,7 +101,7 @@ fetch_template() {
     else
         if [ ! -d "${_template}/.git" ]; then
             mkdir -p "${_template}/"
-            git clone "${_url}" "${_template}" || error_notify "Clone unsuccessful."
+            git clone --branch ${_branch} --single-branch "${_url}" "${_template}" || error_notify "Clone unsuccessful."
         elif [ -d "${_template}/.git" ]; then
             git -C "${_template}" pull ||\
             error_notify "Template update unsuccessful."
@@ -109,11 +110,19 @@ fetch_template() {
     fi
 }
 
-case "${1}" in
-http?://*/*/*)
-    ROCINANTE_TEMPLATE_URL=${1}
-    ROCINANTE_TEMPLATE_USER=$(echo "${1}" | awk -F / '{ print $4 }')
-    ROCINANTE_TEMPLATE_REPO=$(echo "${1}" | awk -F / '{ print $5 }')
-    fetch_template
-    ;;
+if [ $# -lt 1 ]
+  error_exit "Usage: rocinante bootstrap <template_url> [branch]"
+fi
+
+ROCINANTE_TEMPLATE_URL=${1}
+shift
+ROCINANTE_TEMPLATE_BRANCH="${1:-$rocinance_default_branch}"
+
+
+case "${ROCINANTE_TEMPLATE_URL}" in
+    http?://*/*/*)
+        ROCINANTE_TEMPLATE_USER=$(echo "${1}" | awk -F / '{ print $4 }')
+        ROCINANTE_TEMPLATE_REPO=$(echo "${1}" | awk -F / '{ print $5 }')
+        fetch_template
+        ;;
 esac
