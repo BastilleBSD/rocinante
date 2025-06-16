67a429fd7d5ef277dafc0641bd6f908a338bb311
Christer Edwards <cedwards@users.noreply.github.com>
2025-06-15 21:15:27 -0700

    initial attempt to support theme switching & dark vs light theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Rocinante'
 copyright = '2021-2025, Christer Edwards'
@@ -14,12 +8,10 @@ release = '0.1.20220714'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme','sphinx-rtd-dark-mode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -27,18 +19,19 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = '_static/rocinante.jpeg'
 html_theme_options = {
-    'logo_only': False,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    'flyout_display': 'hidden',
-    'version_selector': True,
-    'language_selector': True,
-    # Toc options
     'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
+    'flyout_display': 'hidden',
     'includehidden': True,
-    'titles_only': False
+    'language_selector': True,
+    'logo_only': False,
+    'navigation_depth': 4,
+    'prev_next_buttons_location': 'bottom',
+    'sticky_navigation': True,
+    'style_external_links': False,
+    'style_nav_header_background': 'white',
+    'titles_only': False,
+    'vcs_pageview_mode': '',
+    'theme_switcher': True,
+    'default_mode': 'auto',
+    'version_selector': True,
 }
